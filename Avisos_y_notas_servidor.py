11d9517d3389093fff1eb976545260411f17bd2c
Saul Isaac Gonzalez <120429031+masterred8902@users.noreply.github.com>
2025-05-08 12:45:05 -0600

    Update Avisos_y_notas_servidor.py
    version 1.7

--- Avisos_y_notas_servidor.py
+++ Avisos_y_notas_servidor.py
@@ -111,4 +111,4 @@ def eliminar_aviso(aviso_id: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="192.168.100.16", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
